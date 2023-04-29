70e432f1b3a3233e11846ef656237940b3601a46
root <zchanza@uccs.edu>
2023-04-29 05:06:29 +0000

    more testing

--- spec/features/cards_feature_spec.rb
+++ spec/features/cards_feature_spec.rb
@@ -2,12 +2,35 @@ require "rails_helper"
 
 
 RSpec.feature "Cards", type: :feature do
+    context "Login" do
+        scenario "should sign up" do
+          visit root_path
+          click_link 'Sign up'
+          within("form") do
+            fill_in "Email", with: "testing@test.com"
+            fill_in "Password", with: "123456"
+            fill_in "Password confirmation", with: "123456"
+            click_button "Sign up"
+          end
+          expect(page).to have_content("Welcome! You have signed up successfully.")
+        end
+    
+    
+        scenario "should log in" do
+          user = FactoryBot.create(:user)
+          login_as(user)
+          visit root_path
+          expect(page).to have_content("Logged in")
+        end
+      end
+    
     context "Update card" do
       let(:card) { Card.create(title: "Test title", description: "Test content") }
       before(:each) do
+        user = FactoryBot.create(:user)
+        login_as(user)
         visit edit_card_path(card)
       end
- 
  
       scenario "should be successful" do
         within("form") do
@@ -38,6 +61,8 @@ RSpec.feature "Cards", type: :feature do
     context "Create Card" do
       let(:card) { Card.create(title: "Test title", description: "Test content") }
       before(:each) do
+        user = FactoryBot.create(:user)
+        login_as(user)
         visit new_card_path(card)
       end
  
